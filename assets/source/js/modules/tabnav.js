24a92534ec05abecb9b7c0aa615caf15696cd9c8
Nathan Cardoso <nathan.cardoso@codezone.com.br>
2022-05-17 11:07:36 -0300

    script bracnh refactory-tab-nav

--- assets/source/js/modules/tabnav.js
+++ assets/source/js/modules/tabnav.js
@@ -1,26 +1,3 @@
-// export default function initTabNav() {
-//   const tabmenu = document.querySelectorAll('[data-tab="menu"] li')
-//   const tabcontent = document.querySelectorAll('[data-tab="content"] section')
-
-//   function activeTab(index) {
-//     tabcontent.forEach((section) => {
-//       section.classList.remove('active')
-//     })
-//     const direction = tabcontent[index].dataset.anime
-//     tabcontent[index].classList.add('active', direction)
-//   }
-
-//   if (tabmenu.length && tabcontent.length) {
-//     tabcontent[0].classList.add('active')
-
-//     tabmenu.forEach((item, index) => {
-//       item.addEventListener('click', () => {
-//         activeTab(index)
-//       })
-//     })
-//   }
-// }
-
 export default class TabNav {
   constructor(menu, content) {
     this.tabmenu = document.querySelectorAll(menu)
