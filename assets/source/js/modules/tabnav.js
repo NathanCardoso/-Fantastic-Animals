910d1ddd814652cba415e5ded9c7bc95233c5386
Nathan Cardoso <nathan.cardoso@codezone.com.br>
2022-05-17 10:58:58 -0300

    refactory tabNav to class

--- assets/source/js/modules/tabnav.js
+++ assets/source/js/modules/tabnav.js
@@ -1,22 +1,54 @@
-export default function initTabNav() {
-  const tabmenu = document.querySelectorAll('[data-tab="menu"] li')
-  const tabcontent = document.querySelectorAll('[data-tab="content"] section')
+// export default function initTabNav() {
+//   const tabmenu = document.querySelectorAll('[data-tab="menu"] li')
+//   const tabcontent = document.querySelectorAll('[data-tab="content"] section')
 
-  function activeTab(index) {
-    tabcontent.forEach((section) => {
-      section.classList.remove('active')
-    })
-    const direction = tabcontent[index].dataset.anime
-    tabcontent[index].classList.add('active', direction)
+//   function activeTab(index) {
+//     tabcontent.forEach((section) => {
+//       section.classList.remove('active')
+//     })
+//     const direction = tabcontent[index].dataset.anime
+//     tabcontent[index].classList.add('active', direction)
+//   }
+
+//   if (tabmenu.length && tabcontent.length) {
+//     tabcontent[0].classList.add('active')
+
+//     tabmenu.forEach((item, index) => {
+//       item.addEventListener('click', () => {
+//         activeTab(index)
+//       })
+//     })
+//   }
+// }
+
+export default class TabNav {
+  constructor(menu, content) {
+    this.tabmenu = document.querySelectorAll(menu)
+    this.tabcontent = document.querySelectorAll(content)
+    this.activeClass = 'active'
   }
 
-  if (tabmenu.length && tabcontent.length) {
-    tabcontent[0].classList.add('active')
-
-    tabmenu.forEach((item, index) => {
-      item.addEventListener('click', () => {
-        activeTab(index)
-      })
+  //active tab to according its index
+  activeTab(index) {
+    this.tabcontent.forEach((section) => {
+      section.classList.remove(this.activeClass)
     })
+    const direction = this.tabcontent[index].dataset.anime
+    this.tabcontent[index].classList.add(this.activeClass, direction)
+  }
+
+  //add the events in the tabs
+  addTabNavEvent() {
+    this.tabmenu.forEach((item, index) => {
+      item.addEventListener('click', () => this.activeTab(index))
+    })
+  }
+
+  init() {
+    if (this.tabmenu.length && this.tabcontent.length) {
+      //active first item
+      this.activeTab(0)
+      this.addTabNavEvent()
+    }
   }
 }
--- assets/source/js/script.js
+++ assets/source/js/script.js
@@ -1,7 +1,7 @@
 import SoftScroll from './modules/soft-scroll.js'
 import initAnimationScroll from './modules/animation-scroll.js'
 import Accordion from './modules/accordion.js'
-import initTabNav from './modules/tabnav.js'
+import TabNav from './modules/tabnav.js'
 import initModal from './modules/modal.js'
 import initTooltip from './modules/tooltip.js'
 import initDropdown from './modules/dropdown.js'
@@ -16,8 +16,10 @@ softScroll.init()
 const accordion = new Accordion('[data-anime="accordion"] dt')
 accordion.init()
 
+const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section')
+tabNav.init()
+
 initAnimationScroll()
-initTabNav()
 initModal()
 initTooltip()
 initDropdown()
